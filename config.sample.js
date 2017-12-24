c91d86d499a4edd0d1a728ea4ffef5636397beaf
chen <317755940@qq.com>
2017-12-24 19:39:08 +0800

    config sample update

--- config.sample.js
+++ config.sample.js
@@ -8,7 +8,35 @@ const config = {
 const swagger= {
         uri:config.server.host+":"+config.server.port+'/documentation'
     }
+const mongodb= {
+        uri:'mongodb://localhost/hapi'
+    }
+
+const wx= {
+        appid:'',
+        secret:""
+    }
+
+const microsoftAzure= {
+        emotionRecognitionEndPoint:"https://westus.api.cognitive.microsoft.com/emotion/v1.0",
+        emotionkey1:"",
+        emotionkey2:"",
+        faceRecognitionEndPoint:"https://westcentralus.api.cognitive.microsoft.com/face/v1.0",
+        facekey1:"",
+        facekey2:"",
+    }
+
+const facePlus= {
+        faceRecognitionEndPoint:"https://api-cn.faceplusplus.com/facepp/v3",
+        faceKey:"",
+        faceSecret:"_87hW8R3ro9--2Jq",
+    }
 
 config.swagger=swagger;
+config.mongodb=mongodb;
+config.wx=wx;
+config.microsoftAzure=microsoftAzure;
+config.facePlus=facePlus;
+
 
 module.exports = config
